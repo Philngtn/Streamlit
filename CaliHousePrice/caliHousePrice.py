8da2f4000840999e48575bddf3ff7c1b050bb4fb
PhilLaptop <philngtn@gmail.com>
2022-02-23 23:24:15 -0500

    fix req

--- CaliHousePrice/caliHousePrice.py
+++ CaliHousePrice/caliHousePrice.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import shap
 import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn.ensemble import RandomForestRegressor
@@ -74,18 +73,3 @@ def app():
          columns=['lat', 'lon'])
 
     st.map(map)
-
-# Explaining the model's predictions using SHAP values
-# # https://github.com/slundberg/shap
-# explainer = shap.TreeExplainer(model)
-# shap_values = explainer.shap_values(X)
-#
-# st.header('Feature Importance')
-# plt.title('Feature importance based on SHAP values')
-# shap.summary_plot(shap_values, X)
-# st.pyplot(bbox_inches='tight')
-# st.write('---')
-#
-# plt.title('Feature importance based on SHAP values (Bar)')
-# shap.summary_plot(shap_values, X, plot_type="bar")
-# st.pyplot(bbox_inches='tight')
